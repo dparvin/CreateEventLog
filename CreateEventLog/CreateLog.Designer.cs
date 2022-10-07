da7c14fbbebd5d12e1bc227046fd49abc59b03f9
DaveParvin <DaveParvin@DT-54>
2022-10-07 15:04:18 -0600

    Add project files.

---
+++ CreateEventLog/CreateLog.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CreateEventLog
+{
+    partial class CreateLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateLog));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblEventLogName = new System.Windows.Forms.Label();
+            this.lblSourceName = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtSourceName = new System.Windows.Forms.TextBox();
+            this.txtLogName = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.lblEventLogName, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblSourceName, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnOK, 4, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnCancel, 5, 5);
+            this.tableLayoutPanel1.Controls.Add(this.txtSourceName, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtLogName, 3, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(404, 198);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblEventLogName
+            // 
+            this.lblEventLogName.AutoSize = true;
+            this.lblEventLogName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEventLogName.Location = new System.Drawing.Point(50, 58);
+            this.lblEventLogName.Name = "lblEventLogName";
+            this.lblEventLogName.Padding = new System.Windows.Forms.Padding(4);
+            this.lblEventLogName.Size = new System.Drawing.Size(95, 26);
+            this.lblEventLogName.TabIndex = 0;
+            this.lblEventLogName.Text = "Event Log Name";
+            // 
+            // lblSourceName
+            // 
+            this.lblSourceName.AutoSize = true;
+            this.lblSourceName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblSourceName.Location = new System.Drawing.Point(50, 84);
+            this.lblSourceName.Name = "lblSourceName";
+            this.lblSourceName.Padding = new System.Windows.Forms.Padding(4);
+            this.lblSourceName.Size = new System.Drawing.Size(95, 26);
+            this.lblSourceName.TabIndex = 2;
+            this.lblSourceName.Text = "Source Name";
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(198, 171);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(279, 171);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtSourceName
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtSourceName, 2);
+            this.txtSourceName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSourceName.Location = new System.Drawing.Point(151, 87);
+            this.txtSourceName.Name = "txtSourceName";
+            this.txtSourceName.Size = new System.Drawing.Size(122, 20);
+            this.txtSourceName.TabIndex = 3;
+            // 
+            // txtLogName
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.txtLogName, 2);
+            this.txtLogName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLogName.Location = new System.Drawing.Point(151, 61);
+            this.txtLogName.Name = "txtLogName";
+            this.txtLogName.Size = new System.Drawing.Size(122, 20);
+            this.txtLogName.TabIndex = 1;
+            // 
+            // CreateLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 198);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CreateLog";
+            this.Text = "Create Event Log";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblEventLogName;
+        private System.Windows.Forms.Label lblSourceName;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtSourceName;
+        private System.Windows.Forms.TextBox txtLogName;
+    }
+}
+
---
+++ CreateEventLog/CreateLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CreateEventLog
+{
+    public partial class CreateLog : Form
+    {
+        public CreateLog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtLogName.Text))
+            {
+                var LogName = txtLogName.Text;
+                var SourceName = txtSourceName.Text;
+                if (string.IsNullOrEmpty(SourceName))
+                    SourceName = LogName;
+                try
+                {
+                    var el = new EventLog(LogName, ".", SourceName);
+                    el.WriteEntry("Initializing the event log");
+                    el.MaximumKilobytes = 1024 * 1024;
+                    el.ModifyOverflowPolicy(OverflowAction.OverwriteOlder, 7);
+                    el.Close();
+                    MessageBox.Show("Event Log process done");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("There was an error trying to create the event log: \n\n  " + ex.Message + "\n\nTry running the application in Administrator Mode.");
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
